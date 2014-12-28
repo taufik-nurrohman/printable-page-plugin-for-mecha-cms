9ad5343894b9547255f0fbbe1e03f606b5482cda
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-12-28 23:30:50 +0700

    Allow Users to Rename the Plugin Folder

--- printable-page/configurator.php
+++ printable-page/configurator.php
@@ -1,6 +1,6 @@
 <form class="form-plugin" action="<?php echo $config->url_current; ?>/update" method="post">
   <input name="token" type="hidden" value="<?php echo $token; ?>">
-  <p><?php $the_css = File::open(PLUGIN . DS . 'printable-page' . DS . 'shell' . DS . 'print.css')->read(); echo $speak->plugin_printable_page_description_print_css; ?></p>
+  <p><?php $the_css = File::open(PLUGIN . DS . basename(__DIR__) . DS . 'shell' . DS . 'print.css')->read(); echo $speak->plugin_printable_page_description_print_css; ?></p>
   <p><textarea name="css" class="textarea-block code"><?php echo Text::parse(Guardian::wayback('css', $the_css))->to_encoded_html; ?></textarea></p>
   <p><button class="btn btn-action" type="submit"><i class="fa fa-check-circle"></i> <?php echo $speak->update; ?></button></p>
 </form>
--- printable-page/launch.php
+++ printable-page/launch.php
@@ -3,22 +3,22 @@
 Weapon::add('shield_before', function() {
     $config = Config::get();
     $request = Request::get();
-    if(isset($request['print']) && ($request['print'] == '1' || $request['print'] == 'true')) {
+    if(isset($request['print']) && ((string) $request['print'] == '1' || (string) $request['print'] == 'true')) {
         if($config->page_type == 'article' || $config->page_type == 'page') {
             Filter::add('shield:path', function() {
-                return PLUGIN . DS . 'printable-page' . DS . 'workers' . DS . 'page.php';
+                return PLUGIN . DS . basename(__DIR__) . DS . 'workers' . DS . 'page.php';
             });
         }
     }
 });
 
-Route::accept($config->manager->slug . '/plugin/printable-page/update', function() use($config, $speak) {
+Route::accept($config->manager->slug . '/plugin/' . basename(__DIR__) . '/update', function() use($config, $speak) {
     if( ! Guardian::happy()) {
         Shield::abort();
     }
     if($request = Request::post()) {
         Guardian::checkToken($request['token']);
-        File::write($request['css'])->saveTo(PLUGIN . DS . 'printable-page' . DS . 'shell' . DS . 'print.css');
+        File::write($request['css'])->saveTo(PLUGIN . DS . basename(__DIR__) . DS . 'shell' . DS . 'print.css');
         Notify::success(Config::speak('notify_success_updated', array($speak->plugin)));
         Guardian::kick(dirname($config->url_current));
     }
