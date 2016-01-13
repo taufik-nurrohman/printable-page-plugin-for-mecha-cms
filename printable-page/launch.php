a80580ff9b725a7b251c0075a21f6c67bec4671f
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-01-13 09:07:59 +0700

    $config->is->post

--- printable-page/launch.php
+++ printable-page/launch.php
@@ -1,12 +1,10 @@
 <?php
 
-$print = Request::get('print', false);
-if($print === true || $print > 0) {
-    if(Mecha::walk(glob(POST . DS . '*', GLOB_NOSORT | GLOB_ONLYDIR))->has(POST . DS . $config->page_type)) {
-        Filter::remove($config->page_type . ':content', 'do_toc'); // Remove TOC filter ...
-        Filter::remove($config->page_type . ':content', 'do_page_splitter'); // Remove page splitter filter ...
-        Filter::add('shield:path', function() {
-            return __DIR__ . DS . 'workers' . DS . 'page.php';
-        });
-    }
+$print = Request::get('print', 0);
+if($config->is->post && ($print === true || $print > 0)) {
+    Filter::remove($config->page_type . ':content', 'do_toc'); // Remove `do_toc` filter
+    Filter::remove($config->page_type . ':content', 'do_page_splitter'); // Remove `do_page_splitter` filter
+    Filter::add('shield:path', function() {
+        return __DIR__ . DS . 'workers' . DS . 'page.php';
+    });
 }
